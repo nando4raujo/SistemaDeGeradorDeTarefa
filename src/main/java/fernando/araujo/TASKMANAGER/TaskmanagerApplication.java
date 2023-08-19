54387d7dadec87ecb070bf4ab64e3b2cc93e2669
nando4raujo <fearaujo2.1k@gmail.com>
2023-08-18 21:30:54 -0300

    commit inicial

---
+++ src/main/java/fernando/araujo/TASKMANAGER/TaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package fernando.araujo.TASKMANAGER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerApplication.class, args);
+	}
+
+}
